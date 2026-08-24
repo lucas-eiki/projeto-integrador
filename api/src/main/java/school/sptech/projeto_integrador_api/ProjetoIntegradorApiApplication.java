3e785aae09dfd81d259596784a674c9753691383
lucas-eiki <lucas.gushiken@sptech.school>
2026-08-24 18:51:33 -0300

    chore: adiciona Spring Boot

---
+++ api/src/main/java/school/sptech/projeto_integrador_api/ProjetoIntegradorApiApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.projeto_integrador_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/school/sptech/projeto_integrador_api/ProjetoIntegradorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.projeto_integrador_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
